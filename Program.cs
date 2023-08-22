c63efeba91d49e515d70e9f606bc96c657b73202
Kenneth Berntsen <kberntsen@cpat.com>
2023-08-22 14:57:26 -0500

    Certificate Template

--- Program.cs
+++ Program.cs
@@ -25,6 +25,10 @@ namespace ConsoleApp
             var invoiceReport = rs.RenderByNameAsync("Invoice", InvoiceData).Result;
             invoiceReport.Content.CopyTo(File.OpenWrite("invoice.pdf"));
 
+            Console.WriteLine("Rendering localy stored template jsreport/data/templates/Invoice into invoice.pdf");
+            var certificateReport = rs.RenderByNameAsync("Certificate", CertificateData).Result;
+            invoiceReport.Content.CopyTo(File.OpenWrite("certificate.pdf"));
+
             Console.WriteLine("Rendering custom report fully described through the request object into customReport.pdf");
             var customReport = rs.RenderAsync(CustomRenderRequest).Result;
             customReport.Content.CopyTo(File.OpenWrite("customReport.pdf"));
@@ -64,6 +68,97 @@ namespace ConsoleApp
             items = new[]
             {
                 new { name = "Website design", price = 300 }
+            }
+        };
+
+        static object CertificateData = new
+        {
+            certificate = new
+            {
+                userID = "kkurbat@cpat.com",
+                name = "Katya Kurbat",
+                employeeID = "",
+                score = 0.0,
+                dateUserStarted = 1673287618319,
+                course = "0109 storyline test",
+                courseCompletion = 1,
+                courseCompletionInstant = 1673879840791,
+                topic = "Chapter 1",
+                topicCompletion = 1,
+                topicCompletionInstant = 1673879840791,
+                topicTotalTime = 1917259,
+                lesson = "0109 storyline test",
+                lessonScore = 0.0,
+                lessonScoreWeight = 0.0,
+                lessonCredit = 0,
+                lessonCompletion = 1,
+                lessonCompletionInstant = 1673879840791,
+                lessonStartInstant = 1673287618319,
+                courseTotalTime = 1917259,
+                leafTotalTime = 1917259,
+                logoURL = "",
+                lmsDisplayName = "  CPaT Test LMS QA",
+                certificateSignature = "//content.click4cbt.com/themes/client/3/certificate/Assinatura_ART_1648668770330.png",
+                certificateSignatureTyped = "James Earl Jones",
+                certificateSignatureTitle = "Daria Harris",
+                certificateText = "",
+                versionNumber = -1.0,
+                versionName = "0109 storyline test",
+                versionInstant = 1673287322382,
+                curriculumStartInstant = 1672876800000,
+                curriculumDuration = 2764799999
+            },
+            certificateJSON = new
+            {
+                id = 3,
+                showLogo = false,
+                logoURL = "",
+                titleInfoSection = new
+                {
+                    showLmsName = true,
+                    lmsName = "cpat 2 lms",
+                    showHeader = false,
+                    header = "Informational Header"
+                },
+                showCertificateTitle = true,
+                showAssignmentTitle = true,
+                showCompletionStatement = true,
+                useAssignmentEndDate = false,
+                summaryInfoSection = new
+                {
+                    showSummaryInfo = true,
+                    showEmployeeID = true,
+                    showPosition = true,
+                    showScore = true,
+                    showAssignmentTotalTime = true,
+                    showBase = true,
+                    showFleet = true,
+                    showDateStarted = true
+                },
+                assignmentListSection = new
+                {
+                    showAssignmentList = true,
+                    showAssignmentListTime = false
+                },
+                certificateTextSection = new
+                {
+                    showCertificateText = true,
+                    certificateText = "This is the certificate text. A certification is a credential that you earn to show that you have specific skills or knowledge.A certification is a credential that you earn to show that you have specific skills or knowledge.A certification is a credential that you earn to show that you have specific skills or knowledge."
+                },
+                signatureSection = new
+                {
+                    signatureKey = 0,
+                    showSignature = true,
+                    showPrintedName = true,
+                    showJobTitle = true,
+                    showDate = true,
+                    printedName = "James Earl Jones",
+                    jobTitle = "Vice President of Training",
+                    signatureURL = "//accesslmsqa.blob.core.windows.net/themes/client/3/certificate/3/signature/0/4146565830_37b6c79333_3k_1665781375156.jpeg"
+                },
+                additionalSignatures = "",
+                templateURL = "//accesslmsqa.blob.core.windows.net/themes/certificate/3/certificate.html",
+                cssURL = "//accesslmsqa.blob.core.windows.net/themes/certificate/custom_default/certificate.css"
             }
         };
     }
---
+++ jsreport/data/templates/Certificate/helpers.js
@@ -0,0 +1,66 @@
+﻿function emptyValue(value) {
+    if (value === '')
+        return '-';
+    else
+        return value;
+}
+
+function millisecondsToTime(milli) {
+    if (!milli)
+        return "-";
+    var numMilli = 0;
+    if (milli.toString() == "N/A") {
+        return "—";
+    }
+    else {
+        numMilli = Number(milli);
+    }
+    if (numMilli == 0) {
+        return "—";
+    }
+    var ms = numMilli % 1000;
+    numMilli = (numMilli - ms) / 1000;
+    var secs = numMilli % 60;
+    numMilli = (numMilli - secs) / 60;
+    var mins = numMilli % 60;
+    var hrs = (numMilli - mins) / 60;
+    var returnFormattedTime = "";
+    if (hrs > 0) {
+        returnFormattedTime = (hrs.toString() + "h " + mins.toString() + "m " + secs.toString() + "s");
+    }
+    else if (mins > 0) {
+        returnFormattedTime = (mins.toString() + "m " + secs.toString() + "s");
+    }
+    else {
+        returnFormattedTime = (secs.toString() + "s");
+    }
+    return returnFormattedTime;
+}
+
+function displayScore(score) {
+    var display = "-";
+    if (score) {
+        if (score > 0) {
+            if (score <= 1) {
+                display = ((score * 100).toFixed(2).toString() + "%");
+            }
+            else if (score > 1 && score <= 100) {
+                display = (score.toFixed(2).toString() + "%");
+            }
+            else if (score > 100) {
+                display = score.toString();
+            }
+        }
+    }
+    if (display.indexOf(".00") > -1) {
+        display = display.replace(".00", "");
+    }
+    return display;
+}
+
+function casualDate(numberDate) {
+    if (numberDate) {
+        return this.dateFormatter.casualDate(numberDate);
+    }
+    return "-";
+}
